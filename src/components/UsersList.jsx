bf3ab3efd82e7ce222caed471d2ffc08ebe112e8
DarwinQ15 <daquintero91@misena.edu.co>
2022-09-19 10:00:38 -0500

    arreglos de la app

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -15,38 +15,37 @@ const UsersList = ({users, getUsers, selectUsers}) => {
     }
 
     return (
-        <ul className='container-users'>
-            {users.map((user) =>(
-                <li key={user.id} className='users-item'>
-                    <div className='users-card'>
-                        <div className='user-info'>
-                            <b className='user-name'>{user.first_name + ' ' + user.last_name}</b>
+        <div>
+            <ul className='container-users'>
+                {users.map((user) =>(
+                    <li key={user.id} className='users-item'>
+                        <div className='users-card'>
+                            <div className='user-info'>
+                                <b className='user-name'>{user.first_name + ' ' + user.last_name}</b>
+                            </div>
+                            <hr />
+                            <div className='user-info'>
+                                <b>Emali:</b>
+                                <p className='p-email'>{user.email}</p>
+                            </div>
+                            <div className='user-info'>
+                            <i class='bx bxs-gift' ></i> <b>Birthday: </b>
+                            <p>{user.birthday}</p>
+                            </div>
+                            <div className='btn-card'>
+                                <button className='btn-edit' onClick={()=>selectUsers(user)}><i class='bx bx-edit-alt'></i></button>
+                                <button className='btn-deleted' onClick={()=> deleteUser(user.id)}><i class='bx bx-trash'></i></button>
+                            </div>
                         </div>
-                        <hr />
-                        <div className='user-info'>
-                            <b>Emali:</b>
-                            <p className='p-email'>{user.email}</p>
-                        </div>
-                        <div className='user-info'>
-                        <i class='bx bxs-gift' ></i> <b>Birthday: </b>
-                        <p>{user.birthday}</p>
-                        </div>
-                        <div className='btn-card'>
-                            <button className='btn-edit' onClick={()=>selectUsers(user)}><i class='bx bx-edit-alt'></i></button>
-                            <button className='btn-deleted' onClick={()=> deleteUser(user.id)}><i class='bx bx-trash'></i></button>
-                        </div>
-                    </div>
-                </li>
-                ))
-            }
+                    </li>
+                    ))
+                }
+                </ul>
             <div className='footer'>
-                <h2>Made with love in Academlo
-                </h2>
-                <h3>
-                Web Developer: Darwin Quintero
-                </h3>
+                <h2>Made with love in Academlo</h2>
+                <h3>Web Developer: Darwin Quintero</h3>
             </div>
-            </ul>
+        </div>
     );
 };
 
